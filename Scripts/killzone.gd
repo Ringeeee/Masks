0a6e1c57ac8ee63bcde56cfbd411082cadce24c8
EdelMilch03 <eric.genzel@gmx.de>
2025-10-12 16:40:31 +0200

    kill zone and damage zone

--- Scripts/killzone.gd
+++ Scripts/killzone.gd
@@ -1,5 +1,13 @@
 extends Area2D
+@onready var timer: Timer = $Timer
+@onready var character_body_2d: CharacterBody2D = $"."
 
 
 func _on_body_entered(body: Node2D) -> void:
+	
 	print ("you are dead")
+	timer.start()
+
+
+func _on_timer_timeout() -> void:
+	get_tree().reload_current_scene()
